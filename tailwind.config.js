062393459350bbb4f290861a2ebe1d69aad56a17
RafifShabi <prasetyorafif11@gmail.com>
2024-08-26 15:49:01 +0700

    Describe the changes in the submodule

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./pages/*.{html,js}"],
+tailwind.config = {
+  darkMode: 'class',
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {}
+  }
 }
